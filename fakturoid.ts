30b3161f514f68bd1fb63a3c943a71f44474d306
Jiří Špác <capajj@gmail.com>
2022-07-24 22:27:34 +0200

    much better filtering of invoices

--- fakturoid.ts
+++ fakturoid.ts
@@ -1,6 +1,7 @@
 import unirest from 'unirest'
 import yaml from 'js-yaml'
 import fs from 'fs'
+import moment from 'moment'
 
 export const getTotalForLastXInvoices = async (numberOfInvoices: number) => {
   const input = yaml.load(fs.readFileSync('./input.yaml', 'utf8')) as any
@@ -21,24 +22,34 @@ export const getTotalForLastXInvoices = async (numberOfInvoices: number) => {
       Host: 'app.fakturoid.cz',
       'Cache-Control': 'no-cache',
       Accept: '*/*',
-      'User-Agent': 'Test (capajj@gmail.com)',
+      'User-Agent': 'adisepo generator (capajj@gmail.com)',
     })
     .send()
     .then(
       (res) => {
-        const { body } = res
+        const body: any[] = res.body
 
-        const lastXInvoices = body.slice(0, numberOfInvoices)
+        const quarterInvoices = body.filter((invoice) => {
+          const fulfillmentDate = moment(invoice.taxable_fulfillment_due)
+          return (
+            fulfillmentDate.quarter() === input.tax.quarter &&
+            fulfillmentDate.year() === input.tax.year
+          )
+        })
+        console.log(
+          'quarter Invoices: ',
+          quarterInvoices.map((invoice) => invoice.number)
+        )
 
         console.log(
-          lastXInvoices.map(({ native_total, paid_at, number }) => {
+          quarterInvoices.map(({ native_total, paid_at, number }) => {
             if (!paid_at) {
               console.warn(`seems like invoice ${number} is not yet paid`)
             }
             return native_total
           })
         )
-        const sum = lastXInvoices.reduce((sum, invoice) => {
+        const sum = quarterInvoices.reduce((sum, invoice) => {
           return sum + Number(invoice.native_total)
         }, 0)
         console.log(`sum of last ${numberOfInvoices} is ${sum} CZK`)
